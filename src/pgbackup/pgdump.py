d7de97fa9378c1be86cecd545de5cee0d202db06
Santosh Kaluva <santoshreddy.kaluva@gmail.com>
2020-03-14 23:51:10 -0400

    add pgdump module

---
+++ src/pgbackup/pgdump.py
@@ -0,0 +1,11 @@
+import sys
+import subprocess
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(err)
+        sys.exit(1)
+
+
