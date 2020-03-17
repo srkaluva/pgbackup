db92833489b87a535ed55ff3e6f1f3255aac2f7d
Santosh Kaluva <santoshreddy.kaluva@gmail.com>
2020-03-16 23:31:21 -0400

    combined all pieces togeteher

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -11,22 +11,28 @@ def create_parser():
             Back up PostgreSQL databases locally or to AWS S3.
             """)
   parser.add_argument("url", help="URL of database to backup")
-  parser.add_argument("--driver",
+  parser.add_argument("--driver","-d",
                      help="how & where to store backup",
                      nargs=2,
+                     metavar=("DRIVER", "DESTINATION"),
                      action=DriverAction,
                      required=True)
   return parser
 
 def main():
   import boto3
+  import time
   from pgbackup import pgdump, storage
   args = create_parser().parse_args()
   dump = pgdump.dump(args.url)
   if args.driver == 's3':
     client = boto3.client('s3')
+    timestamp = time.strftime("%Y-%m-%DT%H:%M", time.localtime())
+    file_name = pgdump.dump_file_name(args.url, timestamp)
+    print(f"Backing datanase up to {args.destination} in S3 as {file_name}")
     storage.s3(client, dump.stdout, args.destination, 'example.sql')
   else:
     outfile = open(args.destination, 'wb')
+    print(f"Backing Database up locally to {outfile.name}")
     storage.local(dump.stdout, outfile)
 
